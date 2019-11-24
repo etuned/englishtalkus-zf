cb43f045b6e7e805098e61d6139bcb1bdbe1fcd2
Edwin Bartunek <edwin@devhart.io>
2019-11-24 22:42:53 +0900

    The first production build with minor changes needed.

---
+++ src/assets/js/app.js
@@ -0,0 +1,26 @@
+import $ from 'jquery';
+import 'what-input';
+
+// Foundation JS relies on a global varaible. In ES6, all imports are hoisted
+// to the top of the file so if we used`import` to import Foundation,
+// it would execute earlier than we have assigned the global variable.
+// This is why we have to use CommonJS require() here since it doesn't
+// have the hoisting behavior.
+window.jQuery = $;
+require('foundation-sites');
+
+// If you want to pick and choose which modules to include, comment out the above and uncomment
+// the line below
+//import './lib/foundation-explicit-pieces';
+
+
+$(document).foundation();
+var prev = 0;
+var $window = $(window);
+var nav = $('.menu-bar');
+
+$window.on('scroll', function(){
+  var scrollTop = $window.scrollTop();
+  nav.toggleClass('hidden', scrollTop > prev);
+  prev = scrollTop;
+});
